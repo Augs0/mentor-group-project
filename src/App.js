96063af43030f820a3ea538fa66e793fbfac6f64
August Aldred <augustaldred@bpp.com>
2020-08-06 09:16:32 +0100

    basic project setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      CONTENT WILL GO HERE
+    </div>
+  );
+}
+
+export default App;
